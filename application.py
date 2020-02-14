b9679ead5f44a5d4839dfa1f853c5513e6bde916
Jaya Shankar Bandaru <jayashankarbandaru@Jayas-MacBook-Pro.local>
2020-02-14 23:04:28 +0530

    tried to use socket again but did not work

--- application.py
+++ application.py
@@ -14,17 +14,10 @@ app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 socketio = SocketIO(app)
 
 
-if not os.getenv("DATABASE_URL"):
-    raise RuntimeError("DATABASE_URL is not set")
-
 # Configure session to use filesystem
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
-
-app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-db.init_app(app)
 
 global username
 @app.route("/",methods=["GET","POST"])
@@ -40,4 +33,4 @@ def send_msg(data):
 
 if __name__ == '__main__':
 
-    app.run()
+    app.run(debug=True,host="0.0.0.0")
